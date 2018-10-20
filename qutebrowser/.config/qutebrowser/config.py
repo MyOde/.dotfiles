4c89e743db4ac1728e01d1e1749bfa46fae6a74e
MyOde <zodproxy@gmail.com>
2018-10-20 19:59:11 +0200

    qutebrowser qwerty keys

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -15,34 +15,15 @@ dlAudioPlaylist = whiteSpaceJoin([spawnYTDL, output, options, playlist, url])
 
 c.bindings.commands = {
     'normal': {
-        'D': 'tab-close',
-        'E': 'tab-prev',
-        'F': None,
-        'I': 'forward',
-        'K': 'search-prev',
-        'L': None,
-        'N': 'tab-next',
-        'P': 'reload -f',
-        'R': None,
-        'T': 'hint all tab',
         'd': None,
-        'e': 'scroll up',
-        'f': None,
         'fe': 'open-editor',
         'da': dlAudio,
         'dA': dlAudioPlaylist,
-        'i': 'scroll right',
-        'k': 'search-next',
-        'l': 'enter-mode insert',
-        'n': 'scroll down',
-        'p': 'reload',
-        'r': None,
-        't': 'hint'
     }
 }
 
 c.content.print_element_backgrounds = False
-c.hints.chars = 'arstdhneio'
+c.hints.chars = 'asdfghjkl'
 c.editor.command = ['emacsclient', '-c', '{}']
 c.url.searchengines = {
     "DEFAULT": "https://www.google.com/search?hl&q={}",
